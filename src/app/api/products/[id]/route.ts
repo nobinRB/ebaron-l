68795d18204706c7b967c6fd16392b2b751bd792
Nobin <nobin@redbaron.in>
2025-02-07 13:14:44 +0530

    final files - es fix16

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { connectToDatabase } from '@/lib/mongodb';
 import Product from '@/models/Product';
-
+// @ts-ignore
 export async function GET(
   _req: NextRequest,
   { params }: { params: { id: string } }
